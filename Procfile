332d7433c00128849dd2e365c4800603a5c12718
John D. Bower <jdbower0525@gmail.com>
2016-12-13 12:50:17 -0500

    Initial Commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn dayinthelifeproj.wsgi —log-file -
---
+++ dayinthelifeapp/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+
+class Choice(models.Model):
+    text = models.CharField(max_length=255)
+
+
+class Student(models.Model):
+    choices = models.ManyToManyField(Choice)
+    user = models.OneToOneField(User)
+
+    def __str__(self):
+        return self.user.username
